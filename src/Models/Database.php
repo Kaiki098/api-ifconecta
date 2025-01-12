db8d9a5273e22b39fbf6f51efc2767189c0c9899
kaiki <kaikialvarengasouza098@gmail.com>
2025-01-12 19:02:37 -0300

    Add environment configuration and database connection handling

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -1,18 +1,27 @@
 <?php
 
 namespace App\Models;
+
 use PDO;
 use PDOException;
 
 class Database
 {
-    
-    public static function getConnection()
+    public static function getConnection(): PDO
     {
+        $host = $_ENV['DB_HOST'];
+        $dbName = $_ENV['DB_NAME'];
+        $username = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASS'];
 
-        $dsn = "mysql:host=localhost;dbname=ifconecta;charset=utf8";
-        $pdo = new PDO($dsn, 'root', '@Kaiki0403');
-        return $pdo;
-
+        try {
+            $dsn = "mysql:host=$host;dbname=$dbName;charset=utf8";
+            $pdo = new PDO($dsn, $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        } catch (PDOException $e) {
+            throw new PDOException("Connection failed: " . $e->getMessage());
+        }
     }
 }
